1d5722fff4324177c6e6556ee098a17431c1c1b0
hoshicoding <stippett@eastsideprep.org>
2022-10-25 16:37:36 -0700

    Small admin and database changes
    Added Chemical and Transaction to admin page. Made a chemical transaction for CaSO4.

--- Test Django App/mysite/chemlogs/admin.py
+++ Test Django App/mysite/chemlogs/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice, Chemical, Transaction
 
-admin.site.register(Question)
+#admin.site.register(Question)
+#admin.site.register(Choice)
+admin.site.register(Chemical)
+admin.site.register(Transaction)

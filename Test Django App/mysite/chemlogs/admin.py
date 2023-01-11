3627c1dfe7b8a0f60a34795e037da1d15c61e9fa
plagality <93016934+plagality@users.noreply.github.com>
2023-01-10 16:49:49 -0800

    Revert "Rework model structure to store by bottle"
    This reverts commit 230bbd6eea564457f20f7660d3aa0ec5a90b223c.

--- Test Django App/mysite/chemlogs/admin.py
+++ Test Django App/mysite/chemlogs/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Chemical, Transaction, Bottle, ChemicalState
+from .models import Chemical, Transaction
 
 admin.site.register(Chemical)
 admin.site.register(Transaction)
-admin.site.register(Bottle)
-admin.site.register(ChemicalState)

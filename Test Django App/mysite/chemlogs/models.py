4a01a79a7d55abece97632c746d7bc7a55609ea9
plagality <93016934+plagality@users.noreply.github.com>
2023-01-10 16:44:01 -0800

    Revert "Starting to change templates bc Bottle exists now"
    This reverts commit 003b9a64697707e5a066f2ce592926a4032ce9c0.

--- Test Django App/mysite/chemlogs/models.py
+++ Test Django App/mysite/chemlogs/models.py
@@ -37,6 +37,17 @@ class Chemical(models.Model):
 
     def __str__(self):
         return self.name
+    
+    # returns the current amount in stock
+    def computeAmount(self):
+        amount = 0
+        for transaction in self.transaction_set.all():
+            amount += transaction.amount
+        return amount
+    
+    # returns transactions, sorted by recency
+    def getTransactions(self):
+        return self.transaction_set.all().order_by('-time')
 
 # used by Chemical and Bottle
 class ChemicalState(models.Model):
@@ -51,13 +62,6 @@ class ChemicalState(models.Model):
     min_thresh = models.IntegerField()
     chemical = models.ForeignKey(Chemical, on_delete=models.CASCADE)
 
-    # returns the current amount in stock
-    def computeAmount(self):
-        amount = 0
-        for transaction in self.chemical.bottletransaction_set.all():
-            amount += transaction.amount
-        return amount
-
 # represents a container of a chemical
 class Bottle(models.Model):
     LOC_CHOICES = [
@@ -70,17 +74,6 @@ class Bottle(models.Model):
     contents = models.ForeignKey(ChemicalState, on_delete=models.CASCADE)
     molarity = models.FloatField(null=True, blank=True) # often not applicable
     id = models.CharField(max_length=2, primary_key=True) # composed of capital letters. may need len=3
-    
-    # returns transactions, sorted by recency
-    def getTransactions(self):
-        return self.transaction_set.all().order_by('-time')
-    
-    # returns the current amount in this bottle
-    def computeAmount(self):
-        amount = 0
-        for transaction in self.transaction_set.all():
-            amount += transaction.amount
-        return amount
 
 class Transaction(models.Model):
     bottle = models.ForeignKey(Bottle, on_delete=models.CASCADE)

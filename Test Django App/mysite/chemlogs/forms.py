d510e55aeb95ce6f353a987a58272656b3d78fb9
hoshicoding <stippett@eastsideprep.org>
2023-01-23 22:19:23 -0800

    Backend improvements
    You can reset the amount (volume/mass) of a container
    
    Transactions now have types, which shows whether it's a standard transaction, or an amount reset, etc.
    
    I didn't work on styling sorry
    
    Also changed the Remove button when creating a transaction -- it no longer negates the slider value but rather changes a variable which is not displayed. I.e. it still works but doesn't convolute UI

--- Test Django App/mysite/chemlogs/forms.py
+++ Test Django App/mysite/chemlogs/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 class TransactionEditForm(forms.Form):
     amount = forms.IntegerField(label="Amount")
+    type = forms.CharField(max_length=10, label="Type")
 
 class TransactionCreateForm(forms.Form):
     trSlide = forms.IntegerField(label="")
+
+# set a new amount for a container
+class ContainerOverrideForm(forms.Form):
+    override_value = forms.IntegerField(label="New Amount")

956e4d39aa6c0d834c06caa2a18127804fd12fd4
plagality <93016934+plagality@users.noreply.github.com>
2023-05-09 16:27:21 -0700

    header link updates

--- Test Django App/mysite/chemlogs/urls.py
+++ Test Django App/mysite/chemlogs/urls.py
@@ -6,10 +6,10 @@ from . import views, updater
 app_name = 'chemlogs'
 urlpatterns = [
     path('testPage/<int:chemical_id>/', login_required(views.testPage), name='testPage'), # unused
-    path('chemical/<int:chemical_id>/', login_required(views.chemical), name='chemical'),
+    path('chemical/<int:chemical_id>/', views.chemical, name='chemical'),
     path('testChem/<int:chemical_id>/', login_required(views.testChem), name='testChem'),
-    path('chemicalSearch/', login_required(views.ChemicalSearch.as_view()), name='chemicalSearch'),
-    path('transaction/<int:transaction_id>/', login_required(views.transaction), name='transaction'),
-    path('container/<str:container_id>/', login_required(views.container), name='container'),
+    path('chemicalSearch/', views.ChemicalSearch.as_view(), name='chemicalSearch'),
+    path('transaction/<int:transaction_id>/', views.transaction, name='transaction'),
+    path('container/<str:container_id>/', views.container, name='container'),
     #path('history/', views.history, name='history') # this content is now in chemicalSearch
 ]

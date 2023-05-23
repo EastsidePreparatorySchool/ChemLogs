3b3df142530ecf72f9962a40f8d80c8dd3ace7c5
plagality <93016934+plagality@users.noreply.github.com>
2023-05-23 15:59:48 -0700

    small ui changes

--- Test Django App/mysite/chemlogs/urls.py
+++ Test Django App/mysite/chemlogs/urls.py
@@ -5,11 +5,11 @@ from . import views, updater
 
 app_name = 'chemlogs'
 urlpatterns = [
-    path('testPage/<int:chemical_id>/', login_required(views.testPage), name='testPage'), # unused
-    path('chemical/<int:chemical_id>/', views.chemical, name='chemical'),
-    path('testChem/<int:chemical_id>/', login_required(views.testChem), name='testChem'),
-    path('chemicalSearch/', views.ChemicalSearch.as_view(), name='chemicalSearch'),
-    path('transaction/<int:transaction_id>/', views.transaction, name='transaction'),
-    path('container/<str:container_id>/', views.container, name='container'),
+    path('chemical/<int:chemical_id>/', login_required(views.chemical), name='chemical'),
+    path('chemicalSearch/', login_required(views.ChemicalSearch.as_view()), name='chemicalSearch'),
+    path('transaction/<int:transaction_id>/', login_required(views.transaction), name='transaction'),
+    path('container/<str:container_id>/', login_required(views.container), name='container'),
     #path('history/', views.history, name='history') # this content is now in chemicalSearch
+    # path('testPage/<int:chemical_id>/', login_required(views.testPage), name='testPage'), # unused
+    # path('testChem/<int:chemical_id>/', login_required(views.testChem), name='testChem'), # unused
 ]

7b5d4df5b71411721b8202c9f551ad5e5820923f
hoshicoding <stippett@eastsideprep.org>
2023-06-01 22:24:16 -0700

    Quarterly notifications get emailed

--- Test Django App/mysite/chemlogs/notify.py
+++ Test Django App/mysite/chemlogs/notify.py
@@ -1,11 +1,40 @@
 # send email notifications
 
+import smtplib
+from email.message import EmailMessage
+from django.contrib.auth import get_user_model
+
 def job():
     from .models import ChemicalState
     low_states = [state for state in ChemicalState.objects.all() if state.needMore()]
     send_email("dlao@eastsideprep.org", low_states)
 
 def send_email(address, low_states):
-    print("Low chemical states:")
+    content = "Hello users of chemLogs,\nHere is an automated snapshot of the chemistry supply."
+    content += "\n\nLow chemical states:"
     for state in low_states:
-        print(state.getNotification())
+        content += state.getNotification()
+    
+    sender = "epschemlogs@gmail.com"
+    password = "fjriypmcuywcnwyp" # warning: do not disclose this password
+    User = get_user_model()
+    receiver_addresses = [user.email for user in User.objects.all()]
+    
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(sender, password)
+
+    # message to be sent
+    message = EmailMessage()
+    message.set_content(content)
+    message['Subject'] = "ChemLogs Quarterly Update"
+    message['From'] = sender
+    message['To'] = receiver_addresses
+
+    s.send_message(message)
+    s.quit()
